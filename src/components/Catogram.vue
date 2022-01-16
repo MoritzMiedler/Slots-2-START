2e9d1b56a72d55dcf75c2c74e8cbd1b80bdf540c
Moritz Miedler <miedler.m03@htlwienwest.at>
2022-01-16 20:06:25 +0100

    Initial commit

---
+++ src/components/Catogram.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+   <!-- Add your code here -->
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    cats: {
+      type: Array,
+    },
+  },
+  data() {
+    return {
+      boss: this.cats.find((el) => el.role == 'Boss'),
+      employees: this.cats.filter((el) => el.role != 'Boss'),
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
